a855cd7edc365f2130482aab73219a91fba8d44a
agustin099 <vodrekgamer@gmail.com>
2023-06-01 18:49:46 -0300

    Menu y botones
    botones para entrar a diferentes escenas y volver al menu principal

---
+++ PeriodismoGame/Assets/_Scripts/BotonGlosario.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BotonGlosario : MonoBehaviour
+{
+    public void CargarNivel()
+    {
+        SceneManager.LoadScene("Glosario");
+    }
+}
